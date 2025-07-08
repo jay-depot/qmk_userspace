3829fdd338cfd5b9286a261e547eece60ad5f946
JD <unleet.42@gmail.com>
2025-07-08 16:30:11 -0400

    Basic backlight coloring

--- users/jay_depot/rgbeffects.c
+++ users/jay_depot/rgbeffects.c
@@ -20,47 +20,54 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
     }
 
     uint8_t layer = get_highest_layer(layer_state);
-    if (layer > 0) {
-        hsv_t hsv = rgb_matrix_get_hsv();
-        switch (get_highest_layer(layer_state)) {
-            case LAYER_GAME:
-                hsv = (hsv_t){HSV_COLOR_GAME};
-                break;
-            case LAYER_NAVIGATION:
-                hsv = (hsv_t){HSV_COLOR_NAVIGATION};
-                break;
-            case LAYER_NUMERAL:
-                hsv = (hsv_t){HSV_COLOR_NUMERAL};
-                break;
-            case LAYER_SYMBOLS:
-                hsv = (hsv_t){HSV_COLOR_SYMBOLS};
-                break;
-            case LAYER_POINTER:
-                hsv = (hsv_t){HSV_COLOR_POINTER};
-                break;
-            case LAYER_FUNCTION:
-                hsv = (hsv_t){HSV_COLOR_FUNCTION};
-                break;
-            case LAYER_OSL:
-                hsv = (hsv_t){HSV_COLOR_OSL};
-                break;
-            default:
-                hsv = (hsv_t){HSV_COLOR_DEFAULT};
-                break;
-        };
 
-        // if (hsv.v > rgb_matrix_get_val()) {
-        //     hsv.v = MIN(rgb_matrix_get_val() + 22, 255);
-        // }
+    hsv_t hsv = rgb_matrix_get_hsv();
+    switch (get_highest_layer(layer_state)) {
+        case LAYER_GAME:
+            hsv = (hsv_t){HSV_COLOR_GAME};
+            break;
+        case LAYER_NAVIGATION:
+            hsv = (hsv_t){HSV_COLOR_NAVIGATION};
+            break;
+        case LAYER_NUMERAL:
+            hsv = (hsv_t){HSV_COLOR_NUMERAL};
+            break;
+        case LAYER_SYMBOLS:
+            hsv = (hsv_t){HSV_COLOR_SYMBOLS};
+            break;
+        case LAYER_POINTER:
+            hsv = (hsv_t){HSV_COLOR_POINTER};
+            break;
+        case LAYER_FUNCTION:
+            hsv = (hsv_t){HSV_COLOR_FUNCTION};
+            break;
+        case LAYER_OSL:
+            hsv = (hsv_t){HSV_COLOR_OSL};
+            break;
+        default:
+            hsv = (hsv_t){HSV_COLOR_DEFAULT};
+            break;
+    };
 
-        rgb_t rgb = hsv_to_rgb(hsv);
+    // if (hsv.v > rgb_matrix_get_val()) {
+    //     hsv.v = MIN(rgb_matrix_get_val() + 22, 255);
+    // }
 
-        for (uint8_t i = led_min; i < led_max; i++) {
-            if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
-                rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-            } else if (index >= led_min && index < led_max && index != NO_LED &&
+    rgb_t rgb = hsv_to_rgb(hsv);
+
+    for (uint8_t i = led_min; i < led_max; i++) {
+        if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_UNDERGLOW)) {
+            rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+        }
+    }
+
+    for (uint8_t row = 0; row < MATRIX_ROWS; ++row) {
+        for (uint8_t col = 0; col < MATRIX_COLS; ++col) {
+            uint8_t index = g_led_config.matrix_co[row][col];
+
+            if (index >= led_min && index < led_max && index != NO_LED &&
             keymap_key_to_keycode(layer, (keypos_t){col,row}) > KC_TRNS) {
-                rgb_matrix_set_color(index, rgb);
+                rgb_matrix_set_color(index, rgb.r, rgb.g, rgb.b);
             }
         }
     }
