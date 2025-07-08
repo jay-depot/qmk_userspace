9c3ffec2cedc62660bf1ec14e061151f38380f91
JD <unleet.42@gmail.com>
2025-07-08 14:50:13 -0400

    Move Game layer down

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/jay_depot/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/jay_depot/keymap.c
@@ -61,6 +61,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       OS_LCTL, OS_LGUI, OS_LSFT,         SPC_NUM, TAB_SYM, ENT_OSL
   ),
 
+/**
+ * \brief Game layer.
+ *
+ * QWERTY layout shifted one column to the right for comfortable WASD positioning.
+ * default one-shot mods and space on thumbs.
+ */
+
+  [LAYER_GAME] = LAYOUT_split_3x5_3(
+        KC_ESC, KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
+        KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
+       XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,    KC_N,    KC_M, KC_COMM,    KC_DOT,
+                      _______, _______, _______,         _______, _______, _______
+  ),
+
 /*
  * Layers used on the Dilemma.
  *
@@ -129,20 +143,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   KC_F7,   KC_F8,   KC_F9,  KC_F12,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   KC_F4,   KC_F5,   KC_F6,  KC_F11,
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,         XXXXXXX,   KC_F1,   KC_F2,   KC_F3,  KC_F10,
-                      _______, _______, _______,         _______, _______, _______
-  ),
-
-/**
- * \brief Game layer.
- *
- * QWERTY layout shifted one column to the right for comfortable WASD positioning.
- * default one-shot mods and space on thumbs.
- */
-
-  [LAYER_GAME] = LAYOUT_split_3x5_3(
-        KC_ESC, KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
-        KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
-       XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,    KC_N,    KC_M, KC_COMM,    KC_DOT,
                       _______, _______, _______,         _______, _______, _______
   ),
 
