147917b8675073c5eae3eeddfe2299cef6b73438
JD <unleet.42@gmail.com>
2025-07-11 18:23:38 -0400

    Missed a spot

--- keyboards/bastardkb/dilemma/3x5_3/keymaps/jay_depot/keymap.c
+++ keyboards/bastardkb/dilemma/3x5_3/keymaps/jay_depot/keymap.c
@@ -68,7 +68,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * default one-shot mods and space on thumbs.
  */
 
-  [LAYER_GAME] = LAYOUT_split_3x5_3(
+  [LAYER_GAMING] = LAYOUT_split_3x5_3(
         KC_ESC, KC_Q,    KC_W,    KC_E,    KC_R,            KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,
         KC_TAB, KC_A,    KC_S,    KC_D,    KC_F,            KC_G,    KC_H,    KC_J,    KC_K,    KC_L,
        XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,            KC_B,    KC_N,    KC_M, KC_COMM,    KC_DOT,
